daa212e0f7366809a5543d9eeac6783c6782edfe
fudosman <fudosman@gmail.com>
2022-11-17 13:11:08 +0100

    added the routes for getting stories

---
+++ server/src/controllers/stories.ts
@@ -0,0 +1,11 @@
+module.exports.getAllStories = async (req, res) => {
+  console.log('implement all the codes for geting all stories from the database')
+}
+
+module.exports.getOneStoryById = async (req, res) => {
+  console.log('implement all the codes for geting one stroy by ID from the database')
+}
+
+module.exports.getOneStoryByEmail = async (req, res) => {
+  console.log('implement all the codes for geting one stroy by email from the database')
+}
