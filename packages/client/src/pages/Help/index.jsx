85de5160a75f77514afa5fc8b0c073fe6a5503ea
Udoka <udochukwukaonyela@gmail.com>
2022-11-17 17:05:06 +0100

    feat: created help component

---
+++ packages/client/src/pages/Help/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import './Help.css';
+
+export const Help = () => {
+  return <div>Help</div>;
+};
+
+// These are the types of the props that are passed to the component
+
+Help.propTypes = {};
+
+// These are their default values
+
+Help.defaultProps = {};
+
+export default Help;
