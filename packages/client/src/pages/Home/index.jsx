a6327ee71705b7d9fe6a347993e14e72476243d7
Udoka <udochukwukaonyela@gmail.com>
2022-11-17 17:04:50 +0100

    feat: created Home component

---
+++ packages/client/src/pages/Home/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import './Home.css';
+
+export const Home = () => {
+  return <div>Home</div>;
+};
+
+// These are the types of the props that are passed to the component
+
+Home.propTypes = {};
+
+// These are their default values
+
+Home.defaultProps = {};
+
+export default Home;
