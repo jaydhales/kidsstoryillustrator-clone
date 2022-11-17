e4c0896e5905d856677cd15472bed19efc513607
Udoka <udochukwukaonyela@gmail.com>
2022-11-17 17:06:08 +0100

    created lgo build templatres

---
+++ packages/client/template/blockName.story.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import blockName from './blockName';
+
+export default {
+    title: 'blockName',
+    component: blockName,
+    argTypes: {
+    },
+};
+
+const Template = (args) => <blockName {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+};
+
+export const Large = Template.bind({});
+Large.args = {
+};
+
+export const Small = Template.bind({});
+Small.args = {
+};
