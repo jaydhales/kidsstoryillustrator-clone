851beb45941ed19cb2972453f5ec2ca47a4311de
AAdevStudios <86613826+AAdevdtudios@users.noreply.github.com>
2022-11-17 12:02:37 +0100

    All setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,11 @@
 import express from 'express';
-import http from 'http';
 
 import { config } from './config/config';
 import Logging from './library/Logger';
 
 const app = express();
 
-app.get('/', (req, res, next) => res.status(200).json({ message: 'Welcome to STORY.AI' }));
+app.get('/', (req, res, next) => res.send({ message: 'Welcome to STORY.AI' }));
 
 app.use((req, res, next) => {
     const error = new Error('Not Found');
@@ -15,4 +14,4 @@ app.use((req, res, next) => {
     return res.status(404).json({ message: error.message });
 });
 
-http.createServer(app).listen(config.server.port, () => Logging.info('Server is running on port ' + config.server.port));
+app.listen(config.server.port, () => Logging.info(`Server running on Port ${config.server.port}`));
