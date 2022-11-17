a2cee7db84d72f86509be5c87286e814d95bc4a7
Udoka <udochukwukaonyela@gmail.com>
2022-11-17 18:14:43 +0100

    feat: created component

---
+++ packages/client/src/components/atoms/BlogPostCard/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import './BlogPostCard.css';
+
+export const BlogPostCard = () => {
+  return <div>BlogPostCard</div>;
+};
+
+// These are the types of the props that are passed to the component
+
+BlogPostCard.propTypes = {};
+
+// These are their default values
+
+BlogPostCard.defaultProps = {};
+
+export default BlogPostCard;
