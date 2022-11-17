008bbc0160c7b4ec5f389657a73c39b82490f013
Udoka <udochukwukaonyela@gmail.com>
2022-11-17 17:04:35 +0100

    feat: created Faq component

---
+++ packages/client/src/pages/FAQ/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import './FAQ.css';
+
+export const FAQ = () => {
+  return <div>FAQ</div>;
+};
+
+// These are the types of the props that are passed to the component
+
+FAQ.propTypes = {};
+
+// These are their default values
+
+FAQ.defaultProps = {};
+
+export default FAQ;
