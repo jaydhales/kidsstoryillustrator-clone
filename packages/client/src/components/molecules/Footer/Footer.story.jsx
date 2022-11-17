7ec1a3e55b91f89bb662ca68e13687511d946893
Udoka <udochukwukaonyela@gmail.com>
2022-11-17 17:02:10 +0100

    created footer componet

---
+++ packages/client/src/components/molecules/Footer/Footer.story.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Footer from '.';
+
+export default {
+    title: 'Footer',
+    component: Footer,
+    argTypes: {
+    },
+};
+
+const Template = (args) => <Footer {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+};
+
+export const Large = Template.bind({});
+Large.args = {
+};
+
+export const Small = Template.bind({});
+Small.args = {
+};
