0586bec63fdf516418e93a2adf36e8dc07298d9e
Udoka <udochukwukaonyela@gmail.com>
2022-11-17 17:03:07 +0100

    feat: created blog page

---
+++ packages/client/src/pages/Blog/Blog.story.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Blog from '.';
+
+export default {
+    title: 'Blog',
+    component: Blog,
+    argTypes: {
+    },
+};
+
+const Template = (args) => <Blog {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+};
+
+export const Large = Template.bind({});
+Large.args = {
+};
+
+export const Small = Template.bind({});
+Small.args = {
+};
