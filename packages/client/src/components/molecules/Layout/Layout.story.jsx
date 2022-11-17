202bbd9b65bdd76cd91028f492b5b9a9695fb067
Udoka <udochukwukaonyela@gmail.com>
2022-11-17 17:01:58 +0100

    feat: create layout component

---
+++ packages/client/src/components/molecules/Layout/Layout.story.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Layout from '.';
+
+export default {
+    title: 'Layout',
+    component: Layout,
+    argTypes: {
+    },
+};
+
+const Template = (args) => <Layout {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+};
+
+export const Large = Template.bind({});
+Large.args = {
+};
+
+export const Small = Template.bind({});
+Small.args = {
+};
---
+++ packages/client/src/components/molecules/Layout/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import './Layout.css';
+
+export const Layout = () => {
+  return <div>Layout</div>;
+};
+
+// These are the types of the props that are passed to the component
+
+Layout.propTypes = {};
+
+// These are their default values
+
+Layout.defaultProps = {};
+
+export default Layout;
