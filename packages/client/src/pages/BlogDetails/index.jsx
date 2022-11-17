f83ea4b8042c5acb81333bce3764a70da8f80303
Udoka <udochukwukaonyela@gmail.com>
2022-11-17 17:03:26 +0100

    feat: created blogDetails page

---
+++ packages/client/src/pages/BlogDetails/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import './BlogDetails.css';
+
+export const BlogDetails = () => {
+  return <div>BlogDetails</div>;
+};
+
+// These are the types of the props that are passed to the component
+
+BlogDetails.propTypes = {};
+
+// These are their default values
+
+BlogDetails.defaultProps = {};
+
+export default BlogDetails;
