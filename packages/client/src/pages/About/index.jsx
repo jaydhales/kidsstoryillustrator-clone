5b7df78b5a009255e1ba394b1b79411c45bc0097
Udoka <udochukwukaonyela@gmail.com>
2022-11-17 17:02:43 +0100

    feat: created footer About component

---
+++ packages/client/src/pages/About/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import './About.css';
+
+export const About = () => {
+  return <div>About</div>;
+};
+
+// These are the types of the props that are passed to the component
+
+About.propTypes = {};
+
+// These are their default values
+
+About.defaultProps = {};
+
+export default About;
