bb3189d9e0affff1be9d78a61ef2016d4bc1d9ba
Udoka <udochukwukaonyela@gmail.com>
2022-11-17 17:05:17 +0100

    feat: created Privacy page

---
+++ packages/client/src/pages/Privacy/Privacy.story.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Privacy from './Privacy';
+
+export default {
+    title: 'Privacy',
+    component: Privacy,
+    argTypes: {
+    },
+};
+
+const Template = (args) => <Privacy {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+};
+
+export const Large = Template.bind({});
+Large.args = {
+};
+
+export const Small = Template.bind({});
+Small.args = {
+};
---
+++ packages/client/src/pages/Privacy/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import './Privacy.css';
+
+export const Privacy = () => {
+  return <div>Privacy</div>;
+};
+
+// These are the types of the props that are passed to the component
+
+Privacy.propTypes = {};
+
+// These are their default values
+
+Privacy.defaultProps = {};
+
+export default Privacy;
