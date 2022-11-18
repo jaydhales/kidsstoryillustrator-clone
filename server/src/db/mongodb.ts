308700eb98010c5fbe5327a2bb2f56777ab57400
giwajossy <giwajosef@gmail.com>
2022-11-18 23:08:31 +0100

    setup / connect to MongoDB

--- server/src/db/mongodb.ts
+++ server/src/db/mongodb.ts
@@ -2,7 +2,7 @@ import { connect } from 'mongoose'
 
 const database = process.env.NODE_ENV as string === 'test' ? process.env.TEST_DB : process.env.PROD_DB
 
-// console.log('database', database)
+console.log('DB', database)
 
 export default connect(database as string)
 .then(()=> console.log('🚀  Connected to database... '))
