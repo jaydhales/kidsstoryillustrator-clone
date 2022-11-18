8fcb9cc1296fae3f7009f8fd9a1ff2542efc2bd0
Jigah Miracle <56768247+MJigah@users.noreply.github.com>
2022-11-18 15:07:41 +0100

    added swagger setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,15 @@
 import express from 'express';
-
 import { config } from './config/config';
 import Logging from './library/Logger';
 import routes from './routes/index';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../openapi.json';
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/', (req, res, next) => res.send({ message: 'Welcome to STORY.AI' }));
 
 app.use('/story', routes )
