d1446f3cd650c89058b53f914e27d630bd1316bc
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2022-11-19 02:44:45 +0100

    FRO-15:  styling : Added styling to navbar

--- packages/client/src/components/molecules/Navbar/index.jsx
+++ packages/client/src/components/molecules/Navbar/index.jsx
@@ -4,16 +4,23 @@ import PropTypes from 'prop-types';
 
 import './Navbar.scss';
 
-export const Navbar = () => {
-  return <div>Navbar</div>;
+export const Navbar = ({ children }) => {
+  return <nav className='Navbar'>
+          <img src = 'logodrillpress.svg' alt = 'logo'/>
+            {children}
+        </nav>;
 };
 
 // These are the types of the props that are passed to the component
 
-Navbar.propTypes = {};
+Navbar.propTypes = {
+  children: PropTypes.element.isRequired,
+};
 
 // These are their default values
 
-Navbar.defaultProps = {};
+Navbar.defaultProps = {
+  
+};
 
 export default Navbar;
--- packages/client/src/pages/ForgotPassword/index.jsx
+++ packages/client/src/pages/ForgotPassword/index.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
+import { Navbar } from '../../components/molecules';
 import './ForgotPassword.scss';
 
 export const ForgotPassword = () => {
-  return <div>ForgotPassword</div>;
+  return <div className='Forgot__Password'>
+            <Navbar> 
+
+            </Navbar>
+              
+        </div>;
 };
 
 // These are the types of the props that are passed to the component
