83b3457a944847c310d202e61006b8c3ed8524f3
Favourshaw <favourvanderwaal@gmail.com>
2022-11-19 21:19:11 +0100

    feat-about Page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -3,9 +3,21 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import "./About.css";
+import AboutUsCard from "../../components/molecules/AboutUsCard";
+import AboutUsHero from "../../components/molecules/AboutUsHero";
+import AboutUsTeam from "../../components/molecules/AboutUsTeam";
+import AboutUsVision from "../../components/molecules/AboutUsVision";
+
 
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="About">
+      <AboutUsHero />
+      <AboutUsVision />
+      <AboutUsCard />
+      <AboutUsTeam />
+    </div>
+  );
 };
 
 // These are the types of the props that are passed to the component
