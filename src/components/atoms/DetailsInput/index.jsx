ca952206515807f39ecbaf844aed8ede6dd11bfc
Tolu-Mals <malomo.tolz@gmail.com>
2022-11-20 01:31:34 +0100

    merge fe-branch

--- src/components/atoms/DetailsInput/index.jsx
+++ src/components/atoms/DetailsInput/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-import './DetailsInput.scss';
+import './DetailsInput.css';
 
 export const DetailsInput = () => {
   return <div>DetailsInput</div>;
--- src/components/molecules/AboutUsVision/index.jsx
+++ src/components/molecules/AboutUsVision/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import PropTypes from "prop-types";
 
-import "./AboutUsVision.css";
+import "./AboutUsVision.scss";
 import visionImg from './visionImg.svg';
 
 export const AboutUsVision = () => {
