4e1d99de1e66858b1e49b443deef25fd93e982d6
Udoka <udochukwukaonyela@gmail.com>
2022-11-20 16:20:27 +0100

    styliung

--- src/components/molecules/DashboardSidebar/index.jsx
+++ src/components/molecules/DashboardSidebar/index.jsx
@@ -3,12 +3,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './DashboardSidebar.scss';
-
+import { Link } from 'react-router-dom';
+const details = [
+  {
+    name: "Projects",
+    icon: "/draft.svg",
+    to: "admin/dashboard/projects"
+  },
+  {
+    name: "Drafts",
+    icon: "/stories.svg",
+    to: "admin/dashboard/drafts"
+  },
+  {
+    name: "Stories",
+    icon: "/edit.svg",
+    to: "admin/dashboard/stories"
+  }
+]
 export const DashboardSidebar = ({ className, ...props }) => {
   return (
     <div className={`${className} DashboardSidebar`} {...props}>
       {/* To do change image later */}
-      <div className="CreateNewStory"><img src='/logo192.png' /> <p>Create New story</p></div>
+      <div className="CreateNewStory"><img src='/Add.svg' /> <p>Create New story</p></div>
+      <div className='sideItems-container'>
+        {
+          details.map(({ icon, name, to }, index) =>
+          <Link to={to} key={name + index}>
+            <div className='sidebarItem'>
+            <img src={icon} />
+            <p>{name}</p>
+              </div>
+          </Link>
+          )
+}
+      </div>
     </div>
   );
 };
