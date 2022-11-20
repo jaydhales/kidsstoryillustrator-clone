9c9a71291a0bc7571b4326faf39ea3d9b4f9cda4
Udoka <udochukwukaonyela@gmail.com>
2022-11-20 13:52:43 +0100

    feat: styling

--- src/components/molecules/DashboardHeader/index.jsx
+++ src/components/molecules/DashboardHeader/index.jsx
@@ -5,7 +5,10 @@ import PropTypes from 'prop-types';
 import './DashboardHeader.scss';
 
 export const DashboardHeader = ({ className, ...props }) => {
-  return <div className={`${className}`}>DashboardHeader</div>;
+  return <div className={`${className} DashboardHeader`}>
+    <div className='head-menu'></div>
+    <img src='/Logo.svg' className='head-logo'/>
+  </div>;
 };
 
 // These are the types of the props that are passed to the component
--- src/pages/AdminDashboard/index.jsx
+++ src/pages/AdminDashboard/index.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-import './AdminDashboard.scss';
+import style from './AdminDashboard.scss';
 import { DashboardTable, ProtectedRoute } from '../../components/molecules';
 
 export const AdminDashboard = () => {
     return (
         <ProtectedRoute>
             <div className="admin-dashboard">
+                <div className='heading-container'>
+                    <div className='heading'>
+                        <h2>USERS LIST</h2>
+                        <span><span>Dashboard</span> <img className='arrow' src="/right.svg" /><span>Users List</span></span>
+                    </div>
+                    <input type="text" placeholder="Search" />
+                </div>
                 <DashboardTable />
             </div>
         </ProtectedRoute>
