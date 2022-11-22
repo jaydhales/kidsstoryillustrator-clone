a9c8830338d9e00c4567a35e37a008696450c387
giwajossy <giwajosef@gmail.com>
2022-11-22 16:47:40 +0100

    Chore: update App metadata // remove joi validator

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,19 +1,16 @@
 import express from 'express';
 import { User } from '../handlers/user';
-import { validateSignin, validateSignup } from '../middleware/validate';
 
 const router = express.Router();
 
 router
     .route('/signup')
-    .post(validateSignup, User.signup)
+    .post(User.signup)
 
 
 router
     .route('/signin')
-    .post(validateSignin, User.signin)
-
-
+    .post(User.signin)
 
 
 export default router
