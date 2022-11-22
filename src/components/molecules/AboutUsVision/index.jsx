815728ee1f2b44626efca2c6aca9c28b08ed5399
Favourshaw <favourvanderwaal@gmail.com>
2022-11-22 20:07:02 +0100

    FE-fix-about-us-style

--- src/components/molecules/AboutUsVision/index.jsx
+++ src/components/molecules/AboutUsVision/index.jsx
@@ -3,31 +3,34 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import "./AboutUsVision.scss";
-import visionImg from './visionImg.svg';
+import visionImg from "../../../assets/About-asset/visionImg.svg";
 
 export const AboutUsVision = () => {
   return (
-    <div className="AboutUsVision">
-      <div className="missionLeft">
-        <div className="missionLeftTop">
-          <p>Our Mission & Vision</p>
+    <div className="AboutVision">
+      <div className="AboutUsVision">
+        <div className="missionLeft">
+          <div className="missionLeftTop">
+            <p>Our Mission & Vision</p>
+          </div>
+          <div className="missionLine">
+            <hr />
+          </div>
+          <div className="missionLeftBottom">
+            <p>
+              We aim to offer our users a means of generating images from text
+              for children’s use to bring about active imagination and
+              visualization.
+              <br />
+              <br />
+              To build a standard software that offer a rich set of features
+              widely used for illustrations to help kids visualize knowledge.
+            </p>
+          </div>
         </div>
-        <div className="missionLine">
-          <hr />
+        <div className="missionRight">
+          <img src={visionImg} alt="Vision" className="mission" />
         </div>
-        <div className="missionLeftBottom">
-          <p>
-            We aim to offer our users a means of generating images from text for
-            children’s use to bring about active imagination and visualization.
-            <br />
-            <br />
-            To build a standard software that offer a rich set of features
-            widely used for illustrations to help kids visualize knowledge.
-          </p>
-        </div>
-      </div>
-      <div className="missionRight">
-        <img src={visionImg} alt="Vision" className="mission"/>
       </div>
     </div>
   );
