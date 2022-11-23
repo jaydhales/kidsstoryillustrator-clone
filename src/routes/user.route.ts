2a498f96ebc38079bc7c2fddb50d6ffd061d3d61
Harrylever <ukanah15thdean@gmail.com>
2022-11-23 12:50:45 +0100

    Updated master for Deployment

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import { User } from '../handlers/user';
+import { validateSignin, validateSignup } from '../middleware/validate';
 
 const router = express.Router();
 
 router
     .route('/signup')
-    .post(User.signup)
+    .post(validateSignup, User.signup)
 
 
 router
     .route('/signin')
-    .post(User.signin)
+    .post(validateSignin, User.signin)
 
 
 export default router
