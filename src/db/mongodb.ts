2f483cd26401579e1aa078c80a5d06c1e8f5a3a6
Jigah Miracle <56768247+MJigah@users.noreply.github.com>
2022-11-24 12:12:40 +0100

    Updated Swagger Story Routes

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -4,5 +4,6 @@ const database = process.env.NODE_ENV as string === 'test' ? process.env.TEST_DB
 
 // console.log('database', database)
 
-export default connect(database as string)
+// export default connect(database as string)
+export default connect('mongodb://localhost/story-teller')
 .then(()=> console.log('🚀  Connected to database... '))
