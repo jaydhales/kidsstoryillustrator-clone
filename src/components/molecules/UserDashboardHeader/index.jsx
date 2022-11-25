56758855ddf910b38fcfb5605dc4b77a54610966
Favourshaw <favourvanderwaal@gmail.com>
2022-11-25 21:19:19 +0100

    FE- side nav button ux

--- src/components/molecules/UserDashboardHeader/index.jsx
+++ src/components/molecules/UserDashboardHeader/index.jsx
@@ -3,19 +3,18 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import "./UserDashboardHeader.scss";
-import dpdbtn from "./dpdbtn.svg"
+import dpdbtn from "./dpdbtn.svg";
 
 export const UserDashboardHeader = () => {
   return (
     <>
       <div className="UserDashboardHeader">
         <div className="dashHead">
-          <a href="#home">My Story</a>
-
+          <div className="story">
+            <a href="#home">My Story</a>
+          </div>
           <div className="dropdown">
-            <button className="dropbtn">
-              Dropdown
-            </button>
+            <button className="dropbtn">This Week</button>
             <div className="dropdown-content">
               <a href="#">Link 1</a>
               <a href="#">Link 2</a>
--- src/components/molecules/UserDashboardSide/index.jsx
+++ src/components/molecules/UserDashboardSide/index.jsx
@@ -7,7 +7,14 @@ import Logo from "../../../assets/img/logo.png";
 import UserDashboardHeader from "../UserDashboardHeader";
 import UserDashboardMain from "../UserDashboardMain";
 import hamburger from "./hamburger.svg";
+import stories from "./edit.svg";
+import acctimg from "./account.svg";
+import newstory from "./newstory.svg";
+import mystory from "./mystories.svg";
+import lgout from "./logout.svg";
 import { render } from "@testing-library/react";
+import { Link } from "react-router-dom";
+import 'https://kit.fontawesome.com/0f3801f26c.js'
 
 class UserDashboardSide extends Component {
   state = { clicked: false };
@@ -19,35 +26,65 @@ class UserDashboardSide extends Component {
       <>
         <div className="UserDashboardSide">
           <div className="sidebar" id="mySidenav">
-            <a>
-              <img src={Logo} alt="logo" className="logo" />
-            </a>
-            <a className="active" href="#">
-              Create New Story
-            </a>
-            <a href="#">My Stories</a>
-            <a href="#">Stries</a>
-            <a
-              href="javascript:void(0);"
-              className="icon"
-              onClick={this.handleClick}
-            >
-              <img
-                src={hamburger}
-                className={this.state.clicked ? "&times;" : "&#9776;"}
-              />
-            </a>
+            <Link id="linktext" to="/home" className="logo">
+              <img src={Logo} alt="logo" />
+            </Link>
+            <Link id="linktext" to="/home" className="active">
+              <img src={newstory} className="sidebarIcon" /> Create New Story
+            </Link>
+            <Link id="linktext" to="/home" className="mystory">
+              <img src={mystory} className="sidebarIcon" /> My Stories
+            </Link>
+            <Link id="linktext" to="/home" className="stories">
+              <img src={stories} className="sidebarIcon" /> Stories
+            </Link>
+            <Link id="linktext" to="/home" className="account">
+              {" "}
+              <img src={acctimg} className="sidebarIcon" />
+              Account
+            </Link>
+            <Link id="linktext" to="/home" className="upgrade">
+              Upgrade to Premium
+            </Link>
+
+            <Link id="linktext" to="/home" className="logout">
+              <img src={lgout} className="sidebarIcon" />
+              Log Out
+            </Link>
+            <Link id="linktext" className="icon" onClick={this.handleClick}>
+              <i
+                className={
+                  this.state.clicked ? "fa-solid fa-xmark" : "fa-solid fa-bars"
+                }
+              ></i>
+            </Link>
           </div>
           <div
             id="myNav"
             className={this.state.clicked ? "overlay" : "overlayClose"}
           >
             <div className="overlay-content">
-              <a className="active" href="#">
+              <Link id="linktext" to="/home" className="active">
                 Create New Story
-              </a>
-              <a href="#">My Stories</a>
-              <a href="#">Stries</a>
+              </Link>
+              <Link id="linktext" to="/home">
+                My Stories
+              </Link>
+              <Link id="linktext" to="/home">
+                Stories
+              </Link>
+              <Link id="linktext" to="/home">
+                Account
+              </Link>
+              <Link id="linktext" to="/home">
+                Upgrade to Premium
+              </Link>
+              <Link id="linktext" to="/home">
+                My Stories
+              </Link>
+              <Link id="linktext" to="/home">
+                Log Out
+              </Link>
             </div>
           </div>
 
