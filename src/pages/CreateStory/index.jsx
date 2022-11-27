d33cf2a52232c0ea5cfd181228e4a2af8ad47303
Harrylever <ukanah15thdean@gmail.com>
2022-11-27 14:25:41 +0100

    Finished the Post Stories Function

--- src/pages/CreateStory/index.jsx
+++ src/pages/CreateStory/index.jsx
@@ -59,7 +59,7 @@ export const CreateStory = () => {
   const [modalTitle, setModalTitle] = useState('');
   const [modalMessage, setModalMessage] = useState('');
   const [modalStat, setModalStat] = useState('');
-  // const [funcResponse, setFuncResponse] = useState('');
+  const [storyPosting, setStoryPosting] = useState(false);
 
   const getImagesFromServer = async () => {
     setImageGenerationState(true);
@@ -72,33 +72,34 @@ export const CreateStory = () => {
       },
     }).then((response) => {
       console.log(response);
-      if (typeof response.data == 'string') {
-        return response.data
-      } else {
-        console.log(response.data.links_array);
+      if (typeof response.data == 'string' && response.data.toLowerCase().includes('error')) {
+        setModalDisplay('Error', 'You are out of tokens', 'error');
         setImageLoadingState(false);
-        let new_Arry = [];
-        for (let i = 0; i < 9; i++) {
-          new_Arry.push(response.data.links_array[i]);
-        }
-        setGeneratedImages(new_Arry);
+        setImageGenerationState(false);
+        setGeneratedImages([]);
+        return
       }
+      console.log(response.data.links_array);
+      setImageLoadingState(false);
+      let new_Arry = [];
+      for (let i = 0; i < 9; i++) {
+        new_Arry.push(response.data.links_array[i]);
+      }
+      setGeneratedImages(new_Arry);
+    }).catch(err => {
+      console.error(err);
     });
     setImageGenerationState(false);
-    // return funcResponse
   };
 
   const setImageLoadingFunc = () => {
-    console.log(generatedImages.length);
     if (generatedImages.length != 0) {
       let prompt_response = prompt(
         "Are you sure you want to create a new search? Yes or No"
       ).toLowerCase();
-      if (prompt_response.length >= 2) {
+      if (prompt_response.length >= 2 && prompt_response != null) {
         if (prompt_response == "yes") {
-          //
           setGeneratedImages([]);
-
           setPageParagraph("");
           setPageImgLink("");
         } else if (prompt_response == "no") {
@@ -113,7 +114,7 @@ export const CreateStory = () => {
       setModalDisplay('Error', 'Please enter a valid text', 'error');
     } else {
       setImageLoadingState(true);
-      const getReturnMsg = getImagesFromServer();
+      getImagesFromServer();
 
       let time_count = 60;
 
@@ -123,16 +124,6 @@ export const CreateStory = () => {
 
         if (time_count == 0) {
           clearInterval(this_time_counter);
-        }
-        if (time_count < 50 && getReturnMsg != "") {
-          time_count = 1;
-          console.log("interval Cleared")
-          console.log(getReturnMsg);
-          setModalDisplay('Error', 'You are out of tokens', 'error');
-          setImageLoadingState(false);
-          setImageGenerationState(false);
-          setGeneratedImages([]);
-          return
         }
       }, 1000);
     }
@@ -203,12 +194,10 @@ export const CreateStory = () => {
     setPageContent(pageContent.splice(pageIndex, 1));
   };
 
-  const getToCurrentPage = ({ pageIndex }) => {
-    console.log("function Clicked");
-    const currentPage = pageContent;
-    let currentPageText = currentPage[0].pageParagraphText;
-    let currentPageImageUri = currentPage[0].pageParagraphText;
-  };
+  // const getToCurrentPage = ({ pageIndex }) => {
+  //   console.log("function Clicked");
+  //   const currentPage = pageContent;
+  // };
 
   const setModalDisplay = (title, message, modalStat) => {
     setModalTitle(title);
@@ -218,11 +207,13 @@ export const CreateStory = () => {
   }
 
   const PostStory = () => {
+    setStoryPosting(true);
     const posData = {
+      decoded: { _id: "63836095dcec0b9a8d523767" },
       title: storyTitle,
       numberOfPages: pageContent.length,
       scenes: pageContent,
-    }
+    };
 
     axios({
       method: "post",
@@ -230,7 +221,11 @@ export const CreateStory = () => {
       data: posData
     }).then((response) => {
       console.log(response);
-      setModalDisplay('Success', 'Story Saved Successfully', 'succes');
+      setStoryPosting(false);
+      setModalDisplay('Success', 'Story Saved Successfully', 'success');
+      setTimeout(() => {
+        navigate(0);
+      }, 2600)
     }).catch((err) => {
       console.error(err);
       setModalDisplay('Error', 'Story not saved', 'error');
@@ -268,7 +263,7 @@ export const CreateStory = () => {
       ) : null}
       <div className="cs-sub-container">
         <div className="content-01 top-container">
-          <div className="arr-img-cont" onClick={() => navigate('/')} >
+          <div className="arr-img-cont" onClick={() => navigate("/")}>
             <img src={right_arrow} alt="" />
           </div>
           <div className="btn-container-01">
@@ -281,7 +276,15 @@ export const CreateStory = () => {
                 Preview Story
               </button>
             )}
-            <button className="btn-02" onClick={PostStoryVal}>Save story</button>
+            {storyPosting ? (
+              <button className="btn-02" disabled style={{ opacity: '.6' }}>
+                Save story
+              </button>
+            ) : (
+              <button className="btn-02" onClick={PostStoryVal}>
+                Save story
+              </button>
+            )}
           </div>
         </div>
         {!previewState ? (
@@ -327,7 +330,7 @@ export const CreateStory = () => {
                   <div className="text-context-tabs gen-button-container">
                     {imageGenerationState ? (
                       <button
-                      style={{ opacity: '.6' }}
+                        style={{ opacity: ".6" }}
                         className="gen_button_02"
                         disabled
                       >
@@ -412,7 +415,7 @@ export const CreateStory = () => {
                           }
                           alt=""
                           style={{ width: "30px", cursor: "pointer" }}
-                          onClick={() => getToCurrentPage(index)}
+                          // onClick={() => getToCurrentPage(index)}
                         />
                       </div>
                       <p>{index + 1}</p>
@@ -438,7 +441,9 @@ export const CreateStory = () => {
         ) : (
           <div className="content-02 main-container">
             <div className="preview_container_xxd">
-              <p className="previewtitle_s45e">Story Title: <span>{storyTitle}</span></p>
+              <p className="previewtitle_s45e">
+                Story Title: <span>{storyTitle}</span>
+              </p>
             </div>
             {pageContent.map((page, index) => (
               <div key={index}>
