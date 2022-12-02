483362bdfc65b9c91eb1d3998b274a4a5a172c0c
Jigah Miracle <56768247+MJigah@users.noreply.github.com>
2022-12-02 11:14:21 +0100

    nodemailer Setup

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,4 +1,7 @@
 import nodemailer from 'nodemailer';
+import { google } from 'googleapis';
+const OAuth2 = google.auth.OAuth2;
+
 
 const sendEmail = async(email: any, subject: any, text: any) => {
     try {
@@ -13,20 +16,36 @@ const sendEmail = async(email: any, subject: any, text: any) => {
         //     }
         // });
 
+
         const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
+            host: process.env.PORT,
+            port: 587,
+            service: process.env.SERVICE,
             secure: true,
             auth: {
               type: 'OAuth2',
               user: 'jigah4july@gmail.com',
-              clientId: '000000000000-xxx0.apps.googleusercontent.com',
-              clientSecret: 'XxxxxXXxX0xxxxxxxx0XXxX0',
-              refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx',
-              accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x',
+              clientId: '436047847806-1g500jutav81d00hs9388dijsoluftsk.apps.googleusercontent.com',
+              clientSecret: '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',
+              refreshToken: process.env.REFRESH_TOKEN,
+              accessToken: process.env.ACCESS_TOKEN,
               expires: 1484314697598
             }
           });
+
+        // const transporter = nodemailer.createTransport({
+        //     host: process.env.PORT,
+        //     service: process.env.SERVICE,
+        //     port: 465,
+        //     secure: true,
+        //     auth: {
+        //       type: 'OAuth2',
+        //       user: 'jigah4july@gmail.com',
+        //       clientId: '436047847806-1g500jutav81d00hs9388dijsoluftsk.apps.googleusercontent.com',
+        //       clientSecret: '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',
+        //       expires: 1484314697598
+        //     }
+        //   });
 
         await transporter.sendMail({
             from: process.env.USER,
