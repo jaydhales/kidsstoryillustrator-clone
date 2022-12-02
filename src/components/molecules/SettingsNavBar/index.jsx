d698ca9173e450e0af9981ffaf40944ea6d9d6e8
Your Name <christofaokoro@gmail.com>
2022-12-02 05:26:05 +0100

    added the account section

---
+++ src/components/molecules/SettingsNavBar/index.jsx
@@ -0,0 +1,40 @@
+// import React from "react";
+// import "./SettingsNavBar.scss";
+// import { Route, Routes, Router, Switch, Link } from "react-router-dom"
+// import Account from "../../../pages/Account";
+
+// import PropTypes from "prop-types";
+// export const SettingsNavBar = () => {
+//   return (
+//     // <Router>
+//     <div className="SettingsNavBar">
+//          <nav className="list-section">
+//             <li>
+//                 <Link href="/account">Account</Link>
+//             </li>
+//             <li>
+//                 <Link href="/security">Security</Link>
+//             </li>
+//             <li>
+//                 <Link href="/billing">Billing</Link>
+//             </li>
+//         </nav>
+   
+
+//         {/* <Routes>
+//             <Route path="/account" element={<Account/>}/>
+//         </Routes> */}
+//      </div>
+//     // </Router>
+//   )
+// }
+
+// // These are the types of the props that are passed to the component
+
+// SettingsNavBar.propTypes = {};
+
+// // These are their default values
+
+// SettingsNavBar.defaultProps = {};
+
+// export default SettingsNavBar;
