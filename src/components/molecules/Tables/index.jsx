2fac2f18b0b6b2b2c8ef378fd19fd60df4c5ab5e
okikiola94 <71730677+okikiola94@users.noreply.github.com>
2022-12-02 03:54:57 +0100

    feat: connect api endpoint to update userlist

--- src/components/molecules/Tables/index.jsx
+++ src/components/molecules/Tables/index.jsx
@@ -8,15 +8,24 @@ import axios from "axios";
 
 const endpoint ="https://web-production-c992.up.railway.app/users";
 const Table = () => {
-  
-  
+  const[activeStep, setActiveStep] = useState(0);
+const handleNext =() => {
+    setActiveStep(activeStep + 10) 
+}; 
+// const[prevStep, setPrevStep] = useState(0);
+const handlePrev =() => {
+  if (activeStep >9) {
+    setActiveStep(activeStep - 10)  
+  }
+}
   const ConfigData ={
     headers: {
         "Content-type": "application/json",
-        "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzgxMGZjMzVhMDhmMGEyYjQxYTBiODQiLCJ1c2VybmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2Njk4NTA0ODgsImV4cCI6MTY2OTkzNjg4OH0.QsLM8OPPqc4yaRBjoyGAwn-NkELpa9sZxAameRxMDys"
+        "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzgxMGZjMzVhMDhmMGEyYjQxYTBiODQiLCJ1c2VybmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJpc0FkbWluIjp0cnVlLCJpYXQiOjE2Njk5Mzg3OTgsImV4cCI6MTY3MDAyNTE5OH0.ysk6IUEhawNDsFRbBpLcAwexwKCjoAWgwt7l6X-CUig"
 }
 };
 const[tableItems, setTableItems] = useState([]);
+const[queryItems, setQueryItems] = useState([]);
 const [pageData, setpageData] = useState([]);
 const [pageCounter, setpageCounter] =useState(10);
 const [queryInputValue, setqueryInputValue]=useState ("")
@@ -26,9 +35,10 @@ const [queryInputValue, setqueryInputValue]=useState ("")
 useEffect(()=>{ 
   axios.get (endpoint, ConfigData)
         .then((response) => {
-            const userArray = response.data
+            const userArray = response.data.data
             console.log(response.data.data)
                 setTableItems(userArray)
+                setQueryItems(userArray)
 
             })
          }, []);
@@ -52,29 +62,34 @@ useEffect(()=>{
 
 const handleSearchQuery =(e)=>{
   const query = e.target.value;
-
+  console.log(query)
+if (query=="") {
+  setQueryItems(tableItems)
+  
+}
   const result = tableItems.filter((item)=>item.email.includes(query));
   setqueryInputValue(e.target.value)
-    setTableItems(result);
+  console.log(result)
+    setQueryItems(result);
 
 }
 function paginate(a, pageIndex, pageSize) {
-  var endIndex = Math.min((pageIndex + 1) * pageSize, a.length);
-  return a.slice(Math.max(endIndex - pageSize, 0), endIndex);
+  // var endIndex = Math.min((pageIndex + 1) * pageSize, a.length);
+  return a.slice(pageIndex, pageSize );
 }
 
-const paginatedData = tableItems.filter((users) => {
-  if (queryInputValue === "") {
-    return users;
-  } else if (
-    users.email
-      .toString()
-      .toLocaleLowerCase()
-      .includes(queryInputValue.toLocaleLowerCase())
-  ) {
-    return users;
-  }
-});
+// const paginatedData = tableItems.filter((users) => {
+//   if (queryInputValue === "") {
+//     return users;
+//   } else if (
+//     users.email
+//       .toString()
+//       .toLocaleLowerCase()
+//       .includes(queryInputValue.toLocaleLowerCase())
+//   ) {
+//     return users;
+//   }
+// });
 
   return (     
   <div className="table-wrapper">
@@ -91,30 +106,37 @@ const paginatedData = tableItems.filter((users) => {
       </div>
       <div className="pagnation-action">
       <p>Showing page 1-14 of 2000</p>
-        <img src="/assets/images/Vector.svg" alt="previous" 
+        <img onClick={handlePrev} src="/assets/images/Vector.svg" alt="previous" 
         />
-        <img src="/assets/images/Vector-table2.svg" alt="next" />
+        <img onClick={handleNext} src="/assets/images/Vector-table2.svg" alt="next" />
       </div>
     </div>
     
     <div className="table_f-wrap">
 
    <table className="min-w-full_divide-y-divide-gray-200">
-    <th>
-      <td>Email</td>
+    <thead>
+    <tr>
       <td>First Name</td>
       <td>Last Name</td>
-      <td>User Type</td>
-      <td>Location</td>
-    </th>
+      <td>Email</td>
+      <td>Username</td>
+    </tr>
+    </thead>
 
-
-    {paginate(paginatedData, 0, 10).map((row, index) => (
-    <TableRow key={index} data={row} />
+<tbody>
+    {paginate(queryItems, activeStep, activeStep + 10).map((row, index) => (
+      <tr key={row.id}> 
+      <td key={row.id}>{row.firstname}</td>
+      <td key={row.id}>{row.lastname}</td>
+      <td key={row.id}>{row.email}</td>
+      <td key={row.id}>{row.username}</td>
+      </tr>
 
     )
   
 )}
+</tbody>
 
   </table> 
   </div>
