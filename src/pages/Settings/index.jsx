fd90b5e75a9e83af26b6c3ad1ace696a731402de
Your Name <christofaokoro@gmail.com>
2022-12-02 18:37:27 +0100

    mobile view changes

--- src/pages/Settings/index.jsx
+++ src/pages/Settings/index.jsx
@@ -110,7 +110,7 @@
 //             <div className="nav-btns">
 //               <Link to="/account-info">Edit Account</Link>
 //               <Link to="users">Users(21)</Link>
-//               <Link to="/myStories">All Stories</Link> 
+//               <Link to="/myStories">All Stories</Link>
 //               <Link to="/pricing">Subscriptions</Link>
 //             </div>
 //           </div>
@@ -269,49 +269,62 @@
 
 // export default Settings;
 
-
-import React, {useState} from "react";
+import React, { useState } from "react";
 // import SettingsNavBar from "../../components/molecules/SettingsNavBar"
 // import {Route, Routes} from 'react-router-dom'
 import Account from "../Users/Account";
 import Security from "../Users/Security";
-import SideBar from "../../components/molecules/SideBar"
-
+import SideBar from "../../components/molecules/SideBar";
 
 import PropTypes from "prop-types";
 import "./AcctSetup.scss";
 export const Settings = () => {
-  const [account, setAccount]=useState(false)
-  const [security, setSecurity]=useState(false)
+  const [account, setAccount] = useState(true);
+  const [security, setSecurity] = useState(false);
 
-  const viewAcct = ()=> {
-    setAccount(prevState => !prevState)
-    setSecurity(false)
-  }
+  const viewAcct = () => {
+    setAccount((prevState) => !prevState);
+    setSecurity(false);
+  };
 
-  const vewSecurity = ()=> {
-    setSecurity(prevState => !prevState)
-    setAccount(false)
-  }
+  const viewSecurity = () => {
+    setSecurity((prevState) => !prevState);
+    setAccount(false);
+  };
   return (
     <div className="Settings">
-      <div className="side-bar2"><SideBar /></div>
-     
+      <div className="side-bar2">
+        <SideBar />
+      </div>
+
       <div className="wrap-content">
         <h2 className="head-text">Settings</h2>
         <div className="nav-section">
-        <button onClick={viewAcct}>Account</button>
-        <button onClick={vewSecurity}>Security</button>
-        <button>Billing</button>
+          <button onClick={viewAcct}>Account</button>
+          <button onClick={viewSecurity}>Security</button>
+          <button>Billing</button>
+        </div>
+
+        {/* mobile nav */}
+        <div className="mobile-nav">
+          <select className="mob-nav">
+            <option>
+            <button onClick={viewAcct}>Account</button>
+            </option>
+            <option>
+            <button onClick={viewSecurity}>Security</button>
+            </option>
+            <option>Billing</option>
+          </select>
         </div>
         <div>
-          {!account? <Account/>: null}
-          {security? <Security/>: null}
+          {account ? <Account /> : null}
+          {security ? <Security /> : null}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 // These are the types of the props that are passed to the component
 
