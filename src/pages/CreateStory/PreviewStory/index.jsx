4418a9bb829766535033c09d2a1495fd520d6a8f
James Osunkiyesi <jaydhales@gmail.com>
2022-12-03 04:49:54 +0100

    Feat-Built Create Story and Preview page

--- src/pages/CreateStory/PreviewStory/index.jsx
+++ src/pages/CreateStory/PreviewStory/index.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
-import "./PreviewStory.scss";
+import React from "react";
 
 import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
-const PreviewStory = ({pageDetails: {
-	pageParagraphText,
-	pageImglinkUri
-},}) => {
-	return (
-    <div className='sceneWrapper'>
-      <div className='image_container_xxs'>
-        <img src={pageImglinkUri} alt="" />
+const PreviewStory = ({ story, title, back }) => {
+  return (
+    <div className="wrapper preview">
+      <div className="preview-container">
+        <div className="heading">
+          <div className="prev">
+            <button
+              onClick={(e) => {
+                back(false);
+              }}
+            >
+              back
+            </button>
+            <h2>Story Preview</h2>
+          </div>
+
+          <p>{title}</p>
+
+          <p>Page {story[0].page}</p>
+        </div>
+
+        <div className="preview-main">
+          <div className="image">
+            <img src={story[0].content.image} alt={story[0].content.keyword} />
+          </div>
+
+          <p>{story[0].content.paragraph}</p>
+        </div>
       </div>
-      <p>{pageParagraphText}</p>
     </div>
   );
-}
+};
 
 PreviewStory.propTypes = {
-	pageDetails: PropTypes.object
+  story: PropTypes.array,
+  title: PropTypes.string,
+  back: PropTypes.func,
 };
 
 export default PreviewStory;
