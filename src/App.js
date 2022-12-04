58f8a14fae64cf40e0d300e01f3f62d2a19927fc
James Osunkiyesi <jaydhales@gmail.com>
2022-12-04 11:06:55 +0100

    fix: scroll to top on path change

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import "./App.scss";
 import Routes from "./Auth";
 
 const App = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Routes />
