fc00cbf9f3ad676c87029b7fe45b664b2a320541
James Osunkiyesi <jaydhales@gmail.com>
2022-12-06 21:04:54 +0100

    installed sentry

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ import { AuthProvider } from "./contexts/AuthContext";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 
-
 Sentry.init({
   dsn: "https://665708396c8e41deab42b60e9fd1f698@o4504278112993280.ingest.sentry.io/4504278125838337",
   integrations: [new BrowserTracing()],
 
-   Set tracesSampleRate to 1.0 to capture 100%
-   of transactions for performance monitoring.
-   We recommend adjusting this value in production
-   tracesSampleRate: 1.0,
+  //  Set tracesSampleRate to 1.0 to capture 100%
+  //  of transactions for performance monitoring.
+  //  We recommend adjusting this value in production
+
+  tracesSampleRate: 1.0,
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -29,4 +29,3 @@ root.render(
     </AuthProvider>
   </React.StrictMode>
 );
-
