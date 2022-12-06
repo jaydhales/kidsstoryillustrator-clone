e602c3f347777926aaf13a5663af496f78c61a09
Adelakin Adewumi <61943042+Adelakin-Adewumi@users.noreply.github.com>
2022-12-06 11:17:27 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 import { AuthProvider } from "./contexts/AuthContext";
-//import * as Sentry from "@sentry/react";
-//import { BrowserTracing } from "@sentry/tracing";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
 
-//Sentry.init({
-  //dsn: "https://665708396c8e41deab42b60e9fd1f698@o4504278112993280.ingest.sentry.io/4504278125838337",
-  //integrations: [new BrowserTracing()],
+Sentry.init({
+  dsn: "https://665708396c8e41deab42b60e9fd1f698@o4504278112993280.ingest.sentry.io/4504278125838337",
+  integrations: [new BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-//  tracesSampleRate: 1.0,
-//});
+   Set tracesSampleRate to 1.0 to capture 100%
+   of transactions for performance monitoring.
+   We recommend adjusting this value in production
+   tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
