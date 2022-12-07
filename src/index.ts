1d3ae3848912eb72db3c09a00577392ae026b878
Adelakin Adewumi <61943042+Adelakin-Adewumi@users.noreply.github.com>
2022-12-07 10:45:43 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,3 @@ const transaction = Sentry.startTransaction({
   op: "test",
   name: "My First Test Transaction",
 });
-
-setTimeout(() => {
-  try {
-    foo();
-  } catch (e) {
-    Sentry.captureException(e);
-  } finally {
-    transaction.finish();
-  }
-}, 99);
