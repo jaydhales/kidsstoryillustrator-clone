2dd930835df58684b2ca1dd19dcfefaf3951c581
Adelakin Adewumi <61943042+Adelakin-Adewumi@users.noreply.github.com>
2022-12-07 07:30:45 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
 
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+  dsn: "https://5e78f8f26ebd4d8fb35eb960499035c1@o4504278112993280.ingest.sentry.io/4504278125838336",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
