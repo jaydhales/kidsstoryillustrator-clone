befc5524e95f62475c0c5991f8903e0214ded991
Adelakin Adewumi <61943042+Adelakin-Adewumi@users.noreply.github.com>
2022-12-08 13:25:48 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,32 @@
-const Sentry = require("@sentry/node");
-// or use es6 import statements
-// import * as Sentry from '@sentry/node';
+import express from "express";
+import * as Sentry from "@sentry/node";
 
-const Tracing = require("@sentry/tracing");
-// or use es6 import statements
-// import * as Tracing from '@sentry/tracing';
+// or using CommonJS
+// const express = require('express');
+// const Sentry = require('@sentry/node');
 
-Sentry.init({
-  dsn: "https://5e78f8f26ebd4d8fb35eb960499035c1@o4504278112993280.ingest.sentry.io/4504278125838336",
+const app = express();
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+Sentry.init({ dsn: "https://665708396c8e41deab42b60e9fd1f698@o4504278112993280.ingest.sentry.io/4504278125838337" });
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// All controllers should live here
+app.get("/", function rootHandler(req, res) {
+  res.end("Hello world!");
 });
 
-const transaction = Sentry.startTransaction({
-  op: "test",
-  name: "My First Test Transaction",
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
 });
+
+app.listen(5000);
