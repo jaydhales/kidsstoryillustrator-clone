74cba54083aa54731170b934b1b43d4a3cf70771
Adelakin Adewumi <61943042+Adelakin-Adewumi@users.noreply.github.com>
2022-12-09 12:37:59 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // It must be placed above all other 'import' statements
 import * as atatus from 'atatus-nodejs/start'
+atatus.startMonitor();
 // atatus-config.js
 module.exports = {
     licenseKey: "lic_apm_51db627dbb754cf0b0f818535b6bd0aa",
